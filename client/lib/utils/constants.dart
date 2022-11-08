b9418cd9084a3e1b39654defb4fd4f6b3f776630
siyang12knou <siyang12@knou.ac.kr>
2022-11-08 10:44:16 +0900

    8080포트에서 8000으로 변경

--- client/lib/utils/constants.dart
+++ client/lib/utils/constants.dart
@@ -2,7 +2,7 @@ const String pathSearch = "/search";
 const String pathResult = "/result";
 
 const String pathApi = "/api/v1.0";
-const String devHost = "http://localhost:8080";
+const String devHost = "http://localhost:8000";
 
 const String dataErrorMessage = "시스템 문제로 데이터 처리에 실패하였습니다.";
 const String systemErrorMessage = "시스템에 문제가 발생하였습니다.\n관리자에게 문의해주세요.";
