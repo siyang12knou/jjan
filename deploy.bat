448dbc36869fd2526a4773837636f4c4dd9e9ea5
siyang12knou <siyang12@knou.ac.kr>
2022-11-16 11:37:33 +0900

    AWS EC2 배포 스크립트

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,6 @@ cd client
 flutter build web --release
 rmdir /s /q ..\server\src\main\resources\static
 mkdir ..\server\src\main\resources\static
-xcopy build\web ..\server\src\main\resources\static
+xcopy build\web ..\server\src\main\resources\static /E
 cd ..\server
 .\mvnw clean package -DskipTests=true
--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cp -r build/web/* ../server/src/main/resources/static
 cd ../server
 ./mvnw clean package -DskipTests=true
 
-scp -i "~/.ssh/intel.pem" ./target/server-1.0.0.jar ubuntu@ec2-54-180-83-202.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/intel
+scp -i "../../.ssh/intel.pem" ./target/server-1.0.0.jar ubuntu@ec2-54-180-83-202.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/jjan
